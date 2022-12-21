672a7f4dd3aafbc3ccf9d939339fafdf36bbdc5d
HungFai Koo <jhk888@yahoo.com>
2022-12-20 21:26:42 -0800

    exercise blodPage and contactPage

--- test/specs/contact.js
+++ test/specs/contact.js
@@ -1,17 +1,20 @@
-describe("Navigation Menu", () => {
-  it("Fill all the input fields, submit form and assert success message", async () => {
-    //Open URL
-    await browser.url("/contact");
+import ContactPage from "../pages/contact-page";
 
-    //Fill out the input fields
-    await $(".contact-name input").addValue("Test Name");
-    await $(".contact-email input").addValue("test123@mail.com");
-    await $(".contact-phone input").addValue("123456789");
-    await $(".contact-message textarea").addValue("This is a test message");
-    await $("button[type=submit]").click();
+describe("Contact", () => {
+  it("Fill all the input fields, submit form and assert success message", async () => {
+    await ContactPage.open();
+
+    // Fill out the input fields & click submit
+    // await ContactPage.submitForm('Test Name', 'test@mail.com', '123456789', 'This is a test message');
+    await ContactPage.submitForm(
+      "Test Name",
+      "test@mail.com",
+      "123456789",
+      "This is a test message"
+    );
 
     // Assert the success message
-    const successAlert = $("[role='alert']");
+    const successAlert = ContactPage.alertSuccess;
     await expect(successAlert).toHaveTextContaining(
       "Thanks for contacting us! We will be in touch with you shortly"
     );
